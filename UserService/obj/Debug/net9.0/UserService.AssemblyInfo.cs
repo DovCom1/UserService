404a35e62d96432924d7bd97d9870fd589f894d6
Darlitch <errorguy@mail.ru>
2025-10-02 23:59:17 +0700

    ver 0.21 add .gitignore

--- UserService/obj/Debug/net9.0/UserService.AssemblyInfo.cs
+++ UserService/obj/Debug/net9.0/UserService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c261d4dcabcc496d5f23e7d1c4e499f29cc2978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c7e9c00ab2d11a54efdc14a2d311b83a01005c")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
