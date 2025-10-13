d8dd5ee793d0b896028e8710b3d2f44393d587d5
Darlitch <errorguy@mail.ru>
2025-10-13 21:20:49 +0700

    ver 0.3 Model is done

---
+++ UserService.Model/Utilities/EnumDescriptionAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserService.Model.Utilities;
+
+public class EnumDescriptionAttribute(Type enumType) : ValidationAttribute
+{
+    private readonly Type _enumType = enumType;
+
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value == null) return ValidationResult.Success!;
+        
+        var strValue = value.ToString();
+        if (string.IsNullOrEmpty(strValue)) 
+            return new ValidationResult($"Значение для {validationContext.DisplayName} не может быть пустым.");
+        
+        var descriptions = Enum.GetValues(_enumType)
+            .Cast<Enum>()
+            .Select(e => e.GetDescription())
+            .ToList();
+
+        if (!descriptions.Contains(strValue))
+            return new ValidationResult($"Значение '{strValue}' для {validationContext.DisplayName} недопустимо. Допустимые значения: {string.Join(", ", descriptions)}.");
+
+        return ValidationResult.Success!; 
+    }
+}
