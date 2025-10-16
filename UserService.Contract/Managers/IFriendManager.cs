c5ab01619b4f2dfd0c10bd0dee1a26ecd2b1dd13
Darlitch <errorguy@mail.ru>
2025-10-16 21:22:45 +0700

    ver 0.552 Update IFriendManager

--- UserService.Contract/Managers/IFriendManager.cs
+++ UserService.Contract/Managers/IFriendManager.cs
@@ -1,6 +1,18 @@
-﻿namespace UserService.Contract.Managers;
+﻿using UserService.Model.DTO.FriendUser;
+
+namespace UserService.Contract.Managers;
 
 public interface IFriendManager
 {
-    
+    public Task<FriendUserDTO> SendRequestAsync(CreateFriendUserDTO friendUserDto, CancellationToken cancellationToken);
+    public Task<FriendUserDTO> AcceptFriendRequestAsync(UpdateFriendUserDTO friendUserDto,
+        CancellationToken cancellationToken);
+    public Task DeleteAsync(DeleteFriendUserDTO friendUserDto, CancellationToken cancellationToken);
+    public Task<bool> CheckFriendExists(Guid userId, Guid friendId, CancellationToken cancellationToken);
+    public Task<PagedFriendResponseDTO> GetFriendsAsync(Guid userId, int offset, int limit,
+        CancellationToken cancellationToken);
+    public Task<PagedFriendResponseDTO> GetIncomingRequestsAsync(Guid userId, int offset, int limit,
+        CancellationToken cancellationToken);
+    public Task<PagedFriendResponseDTO> GetOutcomingRequestsAsync(Guid userId, int offset, int limit,
+        CancellationToken cancellationToken);
 }
