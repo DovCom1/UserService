50694745b9f9c2c410e9e5859eeb5fb89b44e894
Darlitch <errorguy@mail.ru>
2025-10-17 18:18:53 +0700

    ver 0.64 FriendsController is done

--- UserService.Api/Controllers/FriendsController.cs
+++ UserService.Api/Controllers/FriendsController.cs
@@ -1,6 +1,67 @@
-﻿namespace UserService.Api.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using UserService.Model.DTO.FriendUser;
+using UserService.Service;
 
-public class FriendsController
+namespace UserService.Api.Controllers;
+
+
+[ApiController]
+[Route("api/users/{userId:guid}/[controller]")]
+public class FriendsController(FriendManager friendManager) : ControllerBase
 {
+    [HttpPost("{friendId:guid}")]
+    public async Task<ActionResult<FriendUserDTO>> AddFriend(Guid userId, Guid friendId, CancellationToken ct)
+    {
+        var dto = new CreateFriendUserDTO(userId, friendId);
+        var result = await friendManager.SendRequestAsync(dto, ct);
+        return CreatedAtAction(nameof(CheckFriendExists), new { userId, friendId }, result);
+    }
+
+    [HttpPatch("{friendId:guid}")]
+    public async Task<ActionResult<FriendUserDTO>> AcceptRequest(Guid userId, Guid friendId,
+        [FromBody] UpdateFriendUserDTO dto, CancellationToken ct)
+    {
+        dto = new UpdateFriendUserDTO(userId, friendId, dto.Status);
+        var result = await friendManager.AcceptFriendRequestAsync(dto, ct);
+        return Ok(result);
+    }
+
+    [HttpDelete("{friendId:guid}")]
+    public async Task<ActionResult> DeleteFriend(Guid userId, Guid friendId, CancellationToken ct)
+    {
+        var dto = new DeleteFriendUserDTO(userId, friendId);
+        await friendManager.DeleteAsync(dto, ct);
+        return NoContent();
+    }
     
+    [HttpGet("{friendId:guid}/exists")]
+    public async Task<ActionResult> CheckFriendExists(Guid userId, Guid friendId, CancellationToken ct)
+    {
+        var exists = await friendManager.CheckFriendExists(userId, friendId, ct);
+        return Ok(new { exists });
+    }
+    
+    [HttpGet("")]
+    public async Task<ActionResult<PagedFriendResponseDTO>> GetFriends(Guid userId,
+        CancellationToken ct, [FromQuery] int offset = 0, [FromQuery] int limit = 10)
+    {
+        var result = await friendManager.GetFriendsAsync(userId, offset, limit, ct);
+        return Ok(result);
+    }
+    
+    [HttpGet("requests/incoming")]
+    public async Task<ActionResult<PagedFriendResponseDTO>> GetIncomingRequests(Guid userId,
+        CancellationToken ct, [FromQuery] int offset = 0, [FromQuery] int limit = 10)
+    {
+        var result = await friendManager.GetIncomingRequestsAsync(userId, offset, limit, ct);
+        return Ok(result);
+    }
+    
+    [HttpGet("requests/outgoing")]
+    public async Task<ActionResult<PagedFriendResponseDTO>> GetOutcomingRequests(Guid userId,
+        CancellationToken ct, [FromQuery] int offset = 0, [FromQuery] int limit = 10)
+    {
+        var result = await friendManager.GetOutcomingRequestsAsync(userId, offset, limit, ct);
+        return Ok(result);
+    }
 }
