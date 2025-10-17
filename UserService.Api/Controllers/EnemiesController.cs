8d5799a74fcc1f27f927edde7b8d01d1c1de71ca
Darlitch <errorguy@mail.ru>
2025-10-17 19:03:43 +0700

    ver 0.65 EnemiesController is done

--- UserService.Api/Controllers/EnemiesController.cs
+++ UserService.Api/Controllers/EnemiesController.cs
@@ -1,6 +1,41 @@
-﻿namespace UserService.Api.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using UserService.Contract.Managers;
+using UserService.Model.DTO.EnemyUser;
 
-public class EnemiesController
+namespace UserService.Api.Controllers;
+
+[ApiController]
+[Route("api/users/{userId:guid}/[controller]")]
+public class EnemiesController(IEnemyManager enemyManager) : ControllerBase
 {
+    [HttpPost("{enemyId:guid}")]
+    public async Task<ActionResult<EnemyUserDTO>> AddEnemy(Guid userId, Guid enemyId, CancellationToken ct)
+    {
+        var dto = new CreateEnemyUserDTO(userId, enemyId);
+        var result = await enemyManager.AddAsync(dto, ct);
+        return CreatedAtAction(nameof(CheckEnemyExists), new { userId, enemyId }, result);
+    }
     
+    [HttpDelete("{enemyId:guid}")]
+    public async Task<ActionResult> DeleteEnemy(Guid userId, Guid enemyId, CancellationToken ct)
+    {
+        var dto = new EnemyUserDTO(userId, enemyId);
+        await enemyManager.DeleteAsync(dto, ct);
+        return NoContent();
+    }
+    
+    [HttpGet("{enemyId:guid}/exists")]
+    public async Task<ActionResult> CheckEnemyExists(Guid userId, Guid enemyId, CancellationToken ct)
+    {
+        var exists = await enemyManager.ExistsAsync(userId, enemyId, ct);
+        return Ok(new { exists });
+    }
+
+    [HttpGet("")]
+    public async Task<ActionResult<PagedEnemyResponseDTO>> GetEnemies(Guid userId,
+        CancellationToken ct, [FromQuery] int offset = 0, [FromQuery] int limit = 10)
+    {
+        var result = await enemyManager.GetEnemiesAsync(userId, offset, limit, ct);
+        return Ok(result);
+    }
 }
